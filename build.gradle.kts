d2b8f3e589815af8633d01f247fe3b535eb801d1
Martin Rasel <maddin@bembelnaut.de>
2022-02-08 23:24:23 +0100

    Exposed Datenbank

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,19 @@ plugins {
     application
 }
 
-group = "me.a1772ca"
+group = "de.bembelnaut.udemy"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+val exposedVersion = "0.37.3"
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 }
 
 tasks.test {
